9f27cf41587504ce3a55a27207ccf5cdedcde1b2
prasad <prasadjustin59@gmail.com>
2024-07-09 16:50:06 +0530

    commit

---
+++ RG/src/test/java/com/ac/rg/RgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ac.rg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
